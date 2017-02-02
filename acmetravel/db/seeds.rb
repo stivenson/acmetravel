77176df087096d8a6dfe45cc0e46a1be4435d8e5
Stivenson <stivenson.rpm@gmail.com>
2017-02-02 16:32:50 -0500

    Seeders

--- acmetravel/db/seeds.rb
+++ acmetravel/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Role.create(id: 1, name: 'Administrador')
+Role.create(id: 2, name: 'Empleado')
+
+User.create(cc: 4545333, names: 'Stivenson', surnames: "Rincon", email: "admin1@gmx.com", password: "123456", role_id: 1)
+User.create(cc: 5545333, names: 'Luis', surnames: "Rincon", email: "emple1@gmx.com", password: "123456", role_id: 2)
